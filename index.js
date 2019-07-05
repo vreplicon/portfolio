6cb592fcdef3e81ac2bf294aa26c8d440cc1f2c4
Violet Replicon <vreplicon@gmail.com>
2019-07-05 11:30:42 -0700

    Adding alt tags and finishing details

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 $(function() {
     $('.hamburger').click(e => {
         $('ul').toggleClass('js-miniMenu');
+
+        if ($('ul').hasClass('js-miniMenu')) {
+            $('.hamburger').html('&#9747');
+        } else {
+            $('.hamburger').html('&#9776');
+        } 
     });
 });
